43e5c921621191b9c78d53daeb179cd7255bf2d4
Macbook <macbook@Macbooks-MacBook-Pro-MDL.local>
2021-11-11 21:14:19 -0400

    Entrega de Proyeco

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import Home from "./component/Home.jsx";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -8,8 +9,25 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
+let counter = 0;
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(() => {
+	const one = Math.floor(counter / 1);
+	const two = Math.floor(counter / 10);
+	const three = Math.floor(counter / 100);
+	const four = Math.floor(counter / 1000);
+	const five = Math.floor(counter / 10000);
+	const six = Math.floor(counter / 100000);
+	counter++;
+	ReactDOM.render(
+		<Home
+			numberOne={one}
+			numberTwo={two}
+			numberThree={three}
+			numberFour={four}
+			numberFive={five}
+			numberSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 500);
